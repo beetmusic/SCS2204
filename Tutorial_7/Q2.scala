106c99c8cd2bd063af1c9570ba5c365586243f27
karththikeyan <kkarththi15@gmail.com>
2022-09-13 11:05:35 +0530

    Second commit

--- Tutorial_7/Q2.scala
+++ Tutorial_7/Q2.scala
@@ -4,14 +4,16 @@ object Q2 extends App{
 	var y=new Rational(5,8)
 	var z=new Rational(2,7)
 	
-	println(x)
-/*	def sub(var x,var y):Rational={
-		
-	}*/
+	var a = x-y-z         // (x.sub(y)).sub(z)
+	
+	println(a)
 	
 	class Rational(n:Int,d:Int){
 		def numer = n
 		def denom = d
+		
+		def -(r:Rational) = new Rational(this.numer*r.denom-r.numer*this.denom,this.denom*r.denom)
+		
 		
 		override def toString = numer+"/"+denom
 		
